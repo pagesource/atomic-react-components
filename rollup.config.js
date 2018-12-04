14bdbc4ebd3fe52adb495a4137116807fc71821f
vinkumar2 <vinkumar2@publicisgroupe.net>
2018-12-04 20:03:52 +0530

    New Rollup config to export components as independent packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,31 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
-
 import packageJson from './package.json';
+
+const glob = require('glob');
+
+const components = glob.sync('./lib/components/**/index.js', { cache: true });
+
+const getRollupEntryOutputConfig = (fileList, isOutputNameFolderName = true) =>
+  fileList.map(item => {
+    const path = item.split('/');
+    const outputModuleName = isOutputNameFolderName
+      ? path[path.length - 2]
+      : path[path.length - 1].split('.')[0];
+    return {
+      input: item,
+      output: {
+        format: 'es',
+        file: `./dist/${outputModuleName}.js`,
+      },
+    };
+  });
+
+const entryOutputList = getRollupEntryOutputConfig(components);
 
 const dependencies = packageJson.dependencies || {};
 
-export default {
-  input: 'lib/index.js',
-  output: {
-    file: 'dist/components.js',
-    format: 'es',
-  },
+const shared = {
   plugins: [
     resolve(),
     babel({
@@ -18,5 +33,10 @@ export default {
       presets: [['@babel/preset-env', { modules: false }], 'next'],
     }),
   ],
+  globals: [],
   external: [...Object.keys(dependencies), 'next/link'],
 };
+
+const rollUpConfig = entryOutputList.map(config => ({ ...shared, ...config }));
+
+export default rollUpConfig;
