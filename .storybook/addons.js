0c11932e7b300c702dbb5d868eb8ca8c8188cc4e
vinkumar2 <vinkumar2@publicisgroupe.net>
2019-05-20 22:32:35 +0530

    Added Storybook Add-on Knobs to one of the component, its able to read all flow type props by itself

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,3 +3,4 @@ import '@storybook/addon-links/register';
 import '@storybook/addon-viewport/register';
 import '@storybook/addon-console';
 import '@storybook/addon-options/register';
+import '@storybook/addon-knobs/register';
