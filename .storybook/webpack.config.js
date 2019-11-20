93e9bdb6c2ad0db440ac37e8610fbf73e93fb14e
vinkumar2 <vinkumar2@publicisgroupe.net>
2019-11-21 01:03:03 +0530

    Removed Global CSS imports like flex-grid, normalise etc

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
 
 module.exports = function({ config }) {
   config.module.rules.push({
-    test: /\.story.jsx?$/,
+    test: /\.story.js?$/,
     loaders: [require.resolve('@storybook/source-loader')],
     enforce: 'pre',
   });
