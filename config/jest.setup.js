bb60d80ff1e3e12d652cecec4c74d2d4bceb52b2
Amit Bhardwaj <Kirans_Vasishta@neimanmarcus.com>
2019-11-22 12:54:12 +0530

    issue-55 | Unique ID generator added

--- config/jest.setup.js
+++ config/jest.setup.js
@@ -2,3 +2,14 @@ const { configure } = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
 configure({ adapter: new Adapter() });
+
+global.uniqueIDincrement = 1;
+
+const mockedDate = new Date(2019, 1, 1);
+
+global.Date = class extends Date {
+  constructor() {
+    super();
+    return mockedDate;
+  }
+};
