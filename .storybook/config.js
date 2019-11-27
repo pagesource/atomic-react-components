0897ef35253992cb341bc40c8e080dbea5bfc013
Aman Jain <amajain3@publicisgroupe.net>
2019-11-27 19:28:34 +0530

    Implemented carousel with test cases.

--- .storybook/config.js
+++ .storybook/config.js
@@ -62,7 +62,7 @@ addParameters({
   },
 });
 
-const req = require.context('../lib/components', true, /story\.js$/);
+const req = require.context('../lib/components/molecules/AtomicCarousel', true, /story\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
