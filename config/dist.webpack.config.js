452193d0ad2463bc5e03417877c3470d72aef023
vinkumar2 <vinkumar2@publicisgroupe.net>
2019-12-04 08:45:03 +0530

    Added CSS loader to build

--- config/dist.webpack.config.js
+++ config/dist.webpack.config.js
@@ -22,6 +22,17 @@ module.exports = (distRoot, optimize) => ({
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
     ],
   },
   externals: {
