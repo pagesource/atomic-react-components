99d53b981dd0958e1a3299d8d3824f9628cf3691
Ankush Saini <anksaini1@publicisgroupe.net>
2020-03-27 14:20:45 +0530

    feat(test-lint): test lint
    test lint

---
+++ .commitlintrc.js
@@ -0,0 +1,16 @@
+const typeEnum = require('@commitlint/config-angular-type-enum');
+
+module.exports = {
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': typeEnum.rules['type-enum'],
+  },
+};
