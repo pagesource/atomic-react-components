f732e46ffeaf7984aec1de2f486a76a04db820fb
Ankush Saini <anksaini1@publicisgroupe.net>
2020-03-27 12:14:23 +0530

    feat(update-rule): update rule
    update rule in commit lint json

--- commitLint.config.js
+++ commitLint.config.js
@@ -1,11 +1,33 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-conventionalcommits',
   rules: {
-    'type-enum': [2, 'always', ['feature', 'fix', 'chore', 'ci']],
-    'subject-case': [
-      1,
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
       'always',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case'],
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
     ],
   },
 };
